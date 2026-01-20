db62c6629ca9b8ad78f928a62e4118cdf6774db9
Jon Eric Wagman <jonericwagman@gmail.com>
2026-01-20 09:23:06 +0000

    Increase the size of the light/dark mode button by 50%

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -17,7 +17,7 @@ export function ThemeToggle() {
 
   if (!mounted) {
     // return a placeholder to avoid hydration issues
-    return <Button variant="ghost" size="icon" disabled />
+    return <Button variant="ghost" size="icon" className="h-[3.75rem] w-[3.75rem]" disabled />
   }
 
   const toggleTheme = () => {
@@ -25,9 +25,9 @@ export function ThemeToggle() {
   }
 
   return (
-    <Button variant="ghost" size="icon" onClick={toggleTheme}>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-yellow-500 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <Button variant="ghost" size="icon" onClick={toggleTheme} className="h-[3.75rem] w-[3.75rem]">
+      <Sun className="h-[1.8rem] w-[1.8rem] rotate-0 scale-100 text-yellow-500 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.8rem] w-[1.8rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
