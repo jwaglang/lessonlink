87b169cfdc4f401d8f65d20b919dac7b786e539f
Jon Eric Wagman <jonericwagman@gmail.com>
2026-01-20 09:40:07 +0000

    make the background of the toggle button the same light lavendar color a

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -17,7 +17,7 @@ export function ThemeToggle() {
 
   if (!mounted) {
     // return a placeholder to avoid hydration issues
-    return <Button variant="ghost" size="icon" className="h-24 w-24" disabled />
+    return <Button variant="secondary" size="icon" className="h-24 w-24" disabled />
   }
 
   const toggleTheme = () => {
@@ -25,7 +25,7 @@ export function ThemeToggle() {
   }
 
   return (
-    <Button variant="ghost" size="icon" onClick={toggleTheme} className="h-24 w-24">
+    <Button variant="secondary" size="icon" onClick={toggleTheme} className="h-24 w-24">
       <Sun className="h-20 w-20 rotate-0 scale-100 text-yellow-500 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-20 w-20 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
