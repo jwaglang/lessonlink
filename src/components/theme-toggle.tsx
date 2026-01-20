95a0e7a7655016368565953daa959a3d140a54c9
Jon Eric Wagman <jonericwagman@gmail.com>
2026-01-20 09:28:43 +0000

    I see no difference

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -26,8 +26,8 @@ export function ThemeToggle() {
 
   return (
     <Button variant="ghost" size="icon" onClick={toggleTheme} className="h-[3.75rem] w-[3.75rem]">
-      <Sun className="h-[3.2rem] w-[3.2rem] rotate-0 scale-100 text-yellow-500 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[3.2rem] w-[3.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-14 w-14 rotate-0 scale-100 text-yellow-500 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-14 w-14 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
